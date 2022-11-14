3165fa63d156235bd473cb633b8c2bbdd855b44c
bryandelonghi <113536458+bryandelonghi@users.noreply.github.com>
2022-11-14 13:01:13 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,53 +1,14 @@
 let roll_value = 0
+/**
+ * Extensions
+ * 
+ * 1. show dice on LED
+ * 
+ * 2. Random.. tally on board of each. everyone include their 20 roll results
+ * 
+ * 3. Tournament - best of three - highest roll
+ */
 input.onGesture(Gesture.Shake, function () {
     roll_value = randint(1, 6)
-    if (roll_value == 1) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . # . .
-            . . . . .
-            . . . . .
-            `)
-    } else if (roll_value == 2) {
-        basic.showLeds(`
-            . . . # .
-            . . # . .
-            . . . . .
-            . . # . .
-            . . . . .
-            `)
-    } else if (roll_value == 3) {
-        basic.showLeds(`
-            . . # . .
-            . . . . .
-            . . # . .
-            . . . . .
-            . . # . .
-            `)
-    } else if (roll_value == 4) {
-        basic.showLeds(`
-            . . . . .
-            . # . # .
-            . . . . .
-            . # . # .
-            . . . . .
-            `)
-    } else if (roll_value == 5) {
-        basic.showLeds(`
-            . . . . .
-            . # . # .
-            . . # . .
-            . # . # .
-            . . . . .
-            `)
-    } else if (roll_value == 6) {
-        basic.showLeds(`
-            . # . # .
-            . . . . .
-            . # . # .
-            . . . . .
-            . # . # .
-            `)
-    }
+    basic.showNumber(roll_value)
 })
